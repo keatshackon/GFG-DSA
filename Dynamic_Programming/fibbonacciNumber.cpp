a633efafdc356f89a27045a8c145840686691d15
keatshackon <keatshackon@gmail.com>
2020-09-24 20:49:18 +0530

    add more algos

---
+++ Dynamic_Programming/fibbonacciNumber.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+// Recursive Implementations
+int fNumber(int n)
+{
+	if(n == 0 || n==1)
+	{
+		return n;
+	}
+	return fNumber(n-1) + fNumber(n-2);
+}
+
+
+
+int fNumber(int n)
+{
+	if(n == 0 || n==1)
+	{
+		return n;
+	}
+	return fNumber(n-1) + fNumber(n-2);
+}
+
+
+
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<fNumber(n);
+	return 0;
+}
