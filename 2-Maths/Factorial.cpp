54263b541755eaf2fd6dcabdf95168cba5f86eec
keatshackon <keatshackon@gmail.com>
2020-11-11 07:33:58 +0530

    Factorials.cpp

---
+++ 2-Maths/Factorial.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+/*
+	//METHOD 2:
+	//=========
+
+	ll fact(ll n){
+	if(n == 0){
+			return 1;
+		}
+		return n * fact(n-1);
+	}
+
+*/
+
+
+int main()
+{
+	ll t;
+	cin>>t;
+
+	// METHOD 1:
+	//=========
+
+	// ll fact = 1;
+	// while(t>=1){
+	// 	fact*=t;
+	// 	t--;
+	// }
+
+
+	/*
+		if(t<0){
+			cout<<"Not Defined";
+		}else{
+			cout<<fact(t)<<"\n";		
+		}
+	*/
+	
+
+	return 0;
+}
