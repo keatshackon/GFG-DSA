f0ec9fed7716bf4c667ee39e90f2f8394f895414
keatshackon <keatshackon@gmail.com>
2020-11-26 18:19:20 +0530

    Updated

---
+++ 10-String/e.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main()
+{
+	char *str;
+
+	char man[20];
+
+	str = strcpy(man,"keats salazar");
+
+	cout<<str;
+
+	return 0;
+}
