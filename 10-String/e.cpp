3c43bfce01df1763e0afd6a00a0697e4881ca138
keatshackon <keatshackon@gmail.com>
2020-11-27 08:13:54 +0530

    sorted Frequency file added

--- 10-String/e.cpp
+++ 10-String/e.cpp
@@ -3,15 +3,24 @@ using namespace std;
 typedef long long ll;
 
 
+void fun(int *arr,int n) {
+	
+	for(int i = 0; i < n; i++){
+		cout<<arr[i]<<" ";
+	}
+
+}
+
 int main()
 {
-	char *str;
+	int *arr = new int[10];
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[4] = 3;
+	arr[5] = 4;
 
-	char man[20];
-
-	str = strcpy(man,"keats salazar");
-
-	cout<<str;
+	fun(arr,10);
 
 	return 0;
 }
---
+++ 10-String/frequencySort.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main()
+{
+	string s;
+	std::vector<int> v(26);
+	cin>>s;
+	for(int i=0;i<s.length();i++){
+		v[s[i] - 'a']+=1;
+	}
+
+	for(int i=0;i<26;i++){
+		if(v[i] != 0)
+			cout<<(char)(i + 'a')<<" : "<<v[i]<<"\n";
+	}
+
+	return 0;
+}
